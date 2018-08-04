cbc0100fcad2a7139aa799b962ad870a2a923c16
jiyinyiyong <jiyinyiyong@gmail.com>
2018-08-05 00:49:32 +0800

    change styles to be more readable

--- src/app/util.cljs
+++ src/app/util.cljs
@@ -1,4 +1,13 @@
 
-(ns app.util )
+(ns app.util
+  (:require ["copy-to-clipboard" :as copy]
+            ["shortid" :as shortid]
+            [respo-message.action :as message-action]))
+
+(defn copy-text! [text d!]
+  (copy text)
+  (let [new-token (.generate shortid)]
+    (d! message-action/create {:text "Copied!", :token new-token})
+    (js/setTimeout #(d! message-action/remove-one {:token new-token}) 2000)))
 
 (defn get-env! [property] (aget (.-env js/process) property))
